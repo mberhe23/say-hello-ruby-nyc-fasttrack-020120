10f020773cfd448154127bf1220c32a33d3778eb
Maryan W. Berhe <maryanberhe@gmail.com>
2020-02-01 23:50:22 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,5 +1,5 @@
 # Build your say_hello method here
 # require_relative  'say_hello_spec.rb'
-def say_hello( name = "Ruby Programmer") 
+def say_hello( name = "Ruby Programmer")
   puts "Hello, #{name}!"
 end
